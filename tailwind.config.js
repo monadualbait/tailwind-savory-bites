c08c4ee622f7f25eb8333a56614754f52f46a828
monadualbait <monadualbait@gmail.com>
2024-04-06 19:00:11 +0300

    worked on the css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#c82617",
+          200: "#A41F13",
+          300: "#56100a",
+        },
+        secondary: {
+          50: "#c9cfd6",
+          100: "#454f5b",
+          200: "#292F36",
+          300: "#0e1012",
+        },
+      },
+      fontFamily: {
+        body: ['Nunito']
+      }
+    },
   },
   plugins: [],
 };
